840a863fb1a78ab318f83603f30f7223d2c9941c
Richard Lange <richard@crittercism.com>
2012-04-07 14:38:27 -0400

    plot_movement_stills can now do block-coloring instead of just gradient

--- demos and tests/plot_movement_stills.m
+++ demos and tests/plot_movement_stills.m
@@ -2,10 +2,29 @@
 % August 24 2011
 %
 % Show movement through time using Blue for first and Red for last posture
+%
+% April 7, 2012: added color_segment_indices functionality so that blocks
+% of movement can be colored the same (no gradient)
 
-function h = plot_movement_stills(B, movement, side)
+function h = plot_movement_stills(B, movement, side, color_segment_indices, colors)
     num_postures = size(movement, 1);
     color_order = [linspace(0,1,num_postures)' zeros(num_postures,1) linspace(1,0,num_postures)'];
+    if(nargin > 3)
+        if(nargin < 5)
+            colors = [[0.8 0 0]; [0 0.8 0]; [0 0 0.8]; [0.8 0.8 0]; [0 0.8 0.8]; [0.8 0 0.8]];
+        end
+        color_order = zeros(num_postures, 3);
+        color_segment_indices = [1, color_segment_indices, num_postures];
+        for i = 1:length(color_segment_indices)-1
+            colors_ind = mod(i-1, length(colors))+1;
+            posture_inds = color_segment_indices(i):color_segment_indices(i+1);
+            disp('coloring postures in range')
+            disp(posture_inds);
+            disp('with color')
+            disp(colors(colors_ind, :))
+            color_order(posture_inds, :) = repmat(colors(colors_ind, :), length(posture_inds), 1);
+        end
+    end
     h = plot_obstacles_color(B);
 
     hold all;
